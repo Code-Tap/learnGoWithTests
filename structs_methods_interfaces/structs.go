b63d17ce04a5306a8d66a25fc3402582c9b9f34f
Leigh Wilson <code-tap@leighwilson.xyz>
2021-08-11 23:29:53 +0100

    all tests pass with test tables

--- structs_methods_interfaces/structs.go
+++ structs_methods_interfaces/structs.go
@@ -8,6 +8,11 @@ type Shape interface {
 	Area() float64
 }
 
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
 type Rectangle struct {
 	Width  float64
 	Height float64
@@ -19,6 +24,10 @@ type Circle struct {
 
 func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.Width + rectangle.Height)
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
 }
 
 func (r Rectangle) Area() float64 {
